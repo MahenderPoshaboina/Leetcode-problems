de50a5fe0ee271bae871d91def0e1e89f80a0296
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-07-10 11:55:31 +0530

    Time: 150 ms (91.34%) | Memory: 9.5 MB (99.58%) - LeetSync

---
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        
+        if (sum % 2 != 0) {
+            return false;
+        }
+        
+        int target = sum / 2;
+        
+        // Create a 1D DP array
+        vector<bool> dp(target + 1, false);
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = dp[j] || dp[j - num];
+            }
+        }
+        
+        return dp[target];
+    }
+};
