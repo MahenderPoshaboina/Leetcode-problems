798f7b8c9c375f9496af9ba3c233c062e56e5fa0
Mahender <75716439+MahenderPoshaboina@users.noreply.github.com>
2023-07-18 11:49:03 +0530

    Time: 17 ms (82.60%) | Memory: 48.7 MB (32.84%) - LeetSync

--- 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,48 +1,20 @@
-// class Solution {
-// public:
-//     bool isPalindrome(string s, int i, int j){
-//         while(i<j){
-//             if(s[i++] != s[j--]) return false;
-//         }
-//         return true;
-//     }
-//     int maxPalindromes(string s, int k) {
-//         int ans=0;
-//         int n= s.size();
-//         for(int i=0; i<n; i++){
-//             for(int j=i; i<n; j++){
-//                 int len= j-i+1;
-//                 if(len>k +1) break;
-//                 if(len >=k and isPalindrome(s,i,j) ){
-//                     ans++;
-//                     i=j-1;
-//                     break;
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
+public:
     bool isPalindrome(string s, int i, int j){
         while(i<j){
-            if(s[i++]!=s[j--]){
-                return false;
-            }
+            if(s[i++] != s[j--]) return false;
         }
         return true;
     }
-public:
     int maxPalindromes(string s, int k) {
-        int ans = 0;
-        for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
+        int ans=0;
+        int n= s.length();
+        for(int i=0; i<n; i++){
+            for(int j=i; j<n; j++){
                 int len= j-i+1;
-                if(len>k+1){
-                    break;
-                }
-                if(len>=k and isPalindrome(s, i, j)){
-                    ans++; 
+                if(len>  k+1) break;
+                if(len >=k and isPalindrome(s,i,j) ){
+                    ans++;
                     i=j;
                     break;
                 }
